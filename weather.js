b527dca3c2095331f807173f834b98fc013c444d
Ilija <68541501+ikarpushyn@users.noreply.github.com>
2023-04-06 19:05:59 +0300

    first commit

--- weather.js
+++ weather.js
@@ -1,21 +1,31 @@
 #!/usr/bin/env node
-
 import { getArgs } from './helpers/args.js';
+import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
 
-const initCLI = () => {
-	const args = getArgs(process.argv);
-
-	console.log(args);
-
-	if (args.h) {
-		// help
-	}
-	if (args.s) {
-		// save city
-	}
-	if (args.t) {
-		// save token
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('token saved');
+	} catch (e) {
+		printError(e.message);
 	}
 };
 
+const initCLI = () => {
+	const args = getArgs(process.argv);
+	console.log("initCLI ~ process.argv:", process.argv)
+
+	if (args.h) {
+		printHelp();
+	}
+
+	if (args.s) {
+	}
+
+	if (args.t) {
+		return saveToken('token', args.t);
+	}
+	// Вывести погоду
+};
 initCLI();
